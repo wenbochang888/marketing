1328591b758e5dbbab589db5828704afa1f98fa0
changwenbo <wenbo.chang@huolala.com>
2024-08-23 15:25:15 +0800

    init

---
+++ src/main/java/com/wenbo/marketing/MarketingApplication.java
@@ -0,0 +1,13 @@
+package com.wenbo.marketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketingApplication.class, args);
+    }
+
+}
